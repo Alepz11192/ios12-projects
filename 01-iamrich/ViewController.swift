da0b78f1d9d3393a7a54192ee5058039a8d39e7c
Alex Herrera <alexherrera@MacBook-Pro-de-Macbook.local>
2019-01-06 13:41:28 -0600

    Initial Commit

---
+++ 01-iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by Alex Herrera on 1/6/19.
+//  Copyright © 2019 Alex Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
